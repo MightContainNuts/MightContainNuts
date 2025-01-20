30f9a1782e4ec128b9f49a48b935c77fac895725
Dean <mightcontainnuts@icloud.com>
2025-01-20 11:32:34 +0100

    Create automate_profile_hashnode_article_population.py

---
+++ automate_profile_hashnode_article_population.py
@@ -0,0 +1,59 @@
+import requests
+
+
+# Hashnode API endpoint
+url = "https://gql.hashnode.com"
+
+# GraphQL query
+query = """
+query Publication(
+    $id: ObjectId="6714bcb54bcdc5c7d9980730"
+  ) {
+    publication(
+      id: $id
+    ) {
+      posts(first:3) {
+        edges {
+          node {
+            title,
+            brief,
+            slug,
+            url
+          }
+        }
+      }
+    } 
+  }
+"""
+
+response = requests.post(url, json={"query": query})
+data = response.json()
+posts = data['data']['publication']['posts']['edges']
+
+
+md_content = ""
+for idx, post in enumerate(posts, start =1):
+    md_content += (f"{idx}. [{post['node']['title']}]({post['node']['url']})\n")
+print(md_content)
+
+
+start_marker = "<!-- BEGIN HASHNODE ARTICLES -->"
+end_marker = "<!-- END HASHNODE ARTICLES -->"
+
+with open("README.md", "r") as readme:
+    content = readme.read()
+
+
+start_idx = content.find(start_marker)
+end_idx = content.find(end_marker)
+
+if start_idx != -1 and end_idx != -1:
+    # Update the content between the markers
+    updated_content = (
+        content[:start_idx + len(start_marker)]  # Content before the start marker
+        + "\n" + md_content.strip() + "\n"      # New content
+        + content[end_idx:]                      # Content after the end marker
+    )
+else:
+    # If markers are not found, append the new section at the end
+    updated_content = content + f"\n{start_marker}\n{md_content.strip()}\n{end_marker}\n"
